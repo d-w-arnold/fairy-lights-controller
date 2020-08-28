649edb23ce9bef96eb73ad9c120cd8897d1e7d2a
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-08-28 20:58:07 +0100

    Completed logic for sequence algorithm: Colour

--- src/Colour.java
+++ src/Colour.java
@@ -10,5 +10,25 @@ public class Colour extends SequenceAlgorithm
     public void run(int numOfFairyLights, List<String> colours)
     {
         System.out.println("\nRunning sequence algorithm: " + this.getClass().getName() + " ...\n");
+        while (true) {
+            for (int i = 1; i <= colours.size(); i++) {
+                for (int j = 1; j <= numOfFairyLights; j++) {
+                    if (j % colours.size() == i % colours.size()) {
+                        System.out.println("Light " + j + " " + getColour(j, colours) + " on");
+                    }
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+                for (int j = 1; j <= numOfFairyLights; j++) {
+                    if (j % colours.size() == i % colours.size()) {
+                        System.out.println("Light " + j + " " + getColour(j, colours) + " off");
+                    }
+                }
+            }
+            System.out.println("\n(Algorithm repeating ...)\n");
+        }
     }
 }
